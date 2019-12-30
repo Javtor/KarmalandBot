98b24ca81aa8bb008ae0d3b91356ba39c1548601
Javier Torres <javierandrestorresreyes@gmail.com>
2019-12-29 20:05:29 -0500

    make it bettera

--- bot.py
+++ bot.py
@@ -10,4 +10,4 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 print('this is my twitter bot', flush=True)
-api.update_status('Hoy no cumple años ningún miembro de Karmaland 4' + str(randint(0,600)))
+api.update_status('Hoy no cumple años ningún miembro de Karmalaand 4' + str(randint(0,600)))
