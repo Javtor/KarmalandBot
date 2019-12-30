95b7ebc3e8ecfcd8bbda2302945bd6c0a15ee0a5
Javier Torres <javierandrestorresreyes@gmail.com>
2019-12-29 19:51:26 -0500

    make it better

---
+++ bot.py
@@ -0,0 +1,13 @@
+from random import seed
+from random import randint
+import tweepy
+import time
+from keys import *
+seed(1)
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+print('this is my twitter bot', flush=True)
+api.update_status('Hoy no cumple años ningún miembro de Karmaland 4' + str(randint(0,600)))
