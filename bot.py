81334a3cb2ebc036820246eee72e9bea435399ae
Javier Torres <javierandrestorresreyes@gmail.com>
2019-12-29 20:54:16 -0500

    v1

--- bot.py
+++ bot.py
@@ -2,12 +2,30 @@ from random import seed
 from random import randint
 import tweepy
 import time
+import datetime
 from keys import *
 seed(time.time())
+
+day = datetime.date.today().strftime("%d-%m")
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-print('this is my twitter bot', flush=True)
-api.update_status('Hoy no cumple años ningún miembro de Karmaland 4' + str(randint(0,600)))
+cumples = {
+    "12-04": "Vegetta @vegetta777",
+    "09-05": "Willyrex @WillyrexYT",
+    "21-05": "Alexby @aLexBY11",
+    "03-08": "Luzu @LuzuGames",
+    "13-02": "Rubius @Rubiu5",
+    "26-05": "Mangel @mangelrogel",
+    "06-07": "Fargan @xFaRgAnx",
+    "13-08": "Lolito @LOLiTOFDEZ",
+    "05-11": "Auron @auronplay"
+}
+
+print(day, flush=True)
+msg = "Hoy no cumple años ningún miembro de Karmaland 4"
+if(day in cumples):
+    msg = "Hoy cumple años "+cumples[day]
+api.update_status(msg+ " " + str(randint(0, 600)))
